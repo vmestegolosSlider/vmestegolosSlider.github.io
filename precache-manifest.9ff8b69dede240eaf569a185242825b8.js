536d505861d2859641570b27b3e84d3dba4dede9
SnowShield <Snowshield@users.noreply.github.com>
2018-10-31 20:21:42 +0300

    init gh-pages commit

---
+++ precache-manifest.9ff8b69dede240eaf569a185242825b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c992f0f235a77fe87764",
+    "url": "/js/chunk-vendors.417e10dc.js"
+  },
+  {
+    "revision": "79fabdb8479dde168d8a",
+    "url": "/js/app.e3a7b605.js"
+  },
+  {
+    "revision": "58b1395030d8cfb1d37bbe7e9e692c29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c992f0f235a77fe87764",
+    "url": "/css/chunk-vendors.8ed30331.css"
+  },
+  {
+    "revision": "79fabdb8479dde168d8a",
+    "url": "/css/app.0c83dc1a.css"
+  }
+];
